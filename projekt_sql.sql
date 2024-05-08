c6e2389390e0470e2b9fce85230bcbe319dd37a3
Vojtěch Rauer <vojtech.rauer@gmail.com>
2024-05-08 15:58:01 +0200

    Update projekt_sql.sql
    proměnné pro cenu produktů v letech 2006 a 2018, průměrná mzda v těchto letech vydělená těmito proměnnými

--- projekt_sql.sql
+++ projekt_sql.sql
@@ -1,6 +1,6 @@
 -- průměrná cena za produkty (pro celou republiku) sjednocená na roky + průměrný plat v daných odvětvích sjednocený na roky
-CREATE OR REPLACE TABLE primary_table AS (
-SELECT avg(cp.value) AS value, cpc.name , year(cp.date_to) AS `year`, 'Cena produktu v Kč' AS value_type, 'Produkt' AS name_type
+CREATE OR REPLACE TABLE primary_table AS
+(SELECT avg(cp.value) AS value, cpc.name , year(cp.date_to) AS `year`, 'Cena produktu v Kč' AS value_type, 'Produkt' AS name_type
 FROM czechia_price AS cp
 INNER JOIN czechia_price_category AS cpc
 ON cpc.code = cp.category_code 
@@ -14,7 +14,7 @@ ON cp.industry_branch_code = cpib.code -- zmizí NULL hodnoty, které nesou úda
 WHERE
 	cp.value_type_code = 5958 -- kód pro průměrnou mzdu
 AND cp.calculation_code = 200 -- kód pro přepočtený průměr (přesnější, protože kalkuluje s částečnými úvazky)
-GROUP BY cpib.name, cp.payroll_year );
+GROUP BY cpib.name, cp.payroll_year);
 
 
 -- 1) Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
@@ -44,6 +44,44 @@ WHERE name_type = 'Produkt';
 -- 2000 až 2021 pro mzdy
 SELECT min(`year`), max(`year`)
 FROM primary_table AS pt
-WHERE name_type = 'Odvětví'
+WHERE name_type = 'Odvětví';
 
 -- první porovnatelné období je rok 2006, poslední rok 2018
+
+SET @2006_bread = (SELECT value
+FROM primary_table AS pt
+WHERE name = 'Chléb konzumní kmínový'
+AND `year` = 2006);
+
+SET @2018_bread = (SELECT value
+FROM primary_table AS pt
+WHERE name = 'Chléb konzumní kmínový'
+AND `year` = 2018);
+
+SET @2006_milk = (SELECT value
+FROM primary_table AS pt
+WHERE name = 'Mléko polotučné pasterované'
+AND `year` = 2006);
+
+SET @2018_milk = (SELECT value
+FROM primary_table AS pt
+WHERE name = 'Mléko polotučné pasterované'
+AND `year` = 2018);
+
+-- průměrná mzda v daném odvětví a roce vydělená průměrnými cenami produktů v tomto roce
+WITH payroll_2006 AS (SELECT value, name 
+FROM primary_table AS pt
+WHERE name_type = 'Odvětví'
+AND `year` = 2006),
+	payroll_2018 AS (SELECT value, name 
+FROM primary_table AS pt
+WHERE name_type = 'Odvětví'
+AND `year` = 2018)
+SELECT p06.name,
+	round(p06.value / @2006_milk) AS l_milk_2006,
+	round(p06.value / @2006_bread) AS kg_bread_2018,
+	round(p18.value / @2018_milk) AS l_milk_2006,
+	round(p18.value / @2018_bread) AS kg_bread_2018
+FROM payroll_2006 AS p06
+INNER JOIN payroll_2018 AS p18
+ON p06.name = p18.name;
