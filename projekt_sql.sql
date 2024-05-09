958714986c466e79a17a2993bf581699a9ccdedb
Vojtěch Rauer <vojtech.rauer@gmail.com>
2024-05-09 17:17:57 +0200

    Update projekt_sql.sql
    otázky 3 a 4

--- projekt_sql.sql
+++ projekt_sql.sql
@@ -85,3 +85,47 @@ SELECT p06.name,
 FROM payroll_2006 AS p06
 INNER JOIN payroll_2018 AS p18
 ON p06.name = p18.name;
+
+
+
+-- Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
+-- pohled procentuálního nárůstu ceny produktů oproti předcházejícímu roku
+WITH perc_increase_price AS (
+SELECT
+	name,
+	`year`,
+	ROUND((value - LAG(value) OVER (PARTITION BY name ORDER BY year)) / ((LAG(value) OVER (PARTITION BY name ORDER BY year)) / 100), 2) AS perc_increase
+FROM primary_table AS pt
+WHERE name_type = 'Produkt')
+-- průměrná hodnota procentuálního narůstu ceny produktů, seřazená od nejmenší k největší
+SELECT 
+	name, round(avg(perc_increase), 2) AS avg_perc_increase
+FROM perc_increase_price
+GROUP BY name
+ORDER BY avg_perc_increase;
+
+
+
+4.) Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+WITH
+	increase_price AS -- procentuální nárůst cen produktů oproti předchozímu roku
+		(SELECT
+		name,
+		`year`,
+		ROUND((value - LAG(value) OVER (PARTITION BY name ORDER BY year)) / ((LAG(value) OVER (PARTITION BY name ORDER BY year)) / 100), 2) AS perc_increase_price
+		FROM primary_table AS pt
+		WHERE name_type = 'Produkt'),
+	increase_wage AS -- procentuální nárůst mzdy v daném odvětví oproti předchozímu roku
+		(SELECT
+		name,
+		`year`,
+		ROUND((value - LAG(value) OVER (PARTITION BY name ORDER BY year)) / ((LAG(value) OVER (PARTITION BY name ORDER BY year)) / 100), 2) AS perc_increase_wage
+		FROM primary_table AS pt
+		WHERE name_type = 'Odvětví')
+SELECT avg(perc_increase_price) - avg(perc_increase_wage) AS price_perc_inc, ip.`year` -- rozdíl průměrného narůstu cen produktů v daném roce a průměrného nárůstu mezd všech odvětví
+FROM increase_price AS ip
+INNER JOIN increase_wage AS iw
+ON ip.`year` = iw.`year`
+WHERE ip.`year` BETWEEN 2007 AND 2018
+GROUP BY ip.`year`
+ORDER BY price_perc_inc DESC;
