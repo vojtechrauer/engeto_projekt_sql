1799602d3b858d2870da33ea319c1c8fa6d34b2a
Vojtěch Rauer <vojtech.rauer@gmail.com>
2024-05-17 17:43:48 +0200

    Update projekt_sql.sql

--- projekt_sql.sql
+++ projekt_sql.sql
@@ -1,5 +1,5 @@
 -- průměrná cena za produkty (pro celou republiku) sjednocená na roky + průměrný plat v daných odvětvích sjednocený na roky
-CREATE OR REPLACE TABLE primary_table AS
+CREATE OR REPLACE TABLE t_vojtech_rauer__project_SQL_primary_final AS
 (SELECT avg(cp.value) AS value, cpc.name , year(cp.date_to) AS `year`, 'Cena produktu v Kč' AS value_type, 'Produkt' AS name_type
 FROM czechia_price AS cp
 INNER JOIN czechia_price_category AS cpc
@@ -31,7 +31,7 @@ SELECT
 	name,
 	`year`,
 	ROUND((value - LAG(value) OVER (PARTITION BY name ORDER BY year)) / ((LAG(value) OVER (PARTITION BY name ORDER BY year)) / 100), 2) AS perc_increase
-FROM primary_table AS pt
+FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 WHERE name_type = 'Odvětví')
 -- Výběr odvětví a let, u kterých oproti předcházejícím roku procentuálně poklesla průměrná mzda a procento poklesu
 SELECT 
@@ -44,43 +44,43 @@ ORDER BY perc_increase;
 
 -- časový rozptyl měřených dat -> od roku 2006, do roku 2018 pro ceny produktů
 SELECT min(`year`), max(`year`)
-FROM primary_table AS pt
+FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 WHERE name_type = 'Produkt';
 
 -- 2000 až 2021 pro mzdy
 SELECT min(`year`), max(`year`)
-FROM primary_table AS pt
+FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 WHERE name_type = 'Odvětví';
 
 -- první porovnatelné období je rok 2006, poslední rok 2018
 
 SET @2006_bread = (SELECT value
-FROM primary_table AS pt
+FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 WHERE name = 'Chléb konzumní kmínový'
 AND `year` = 2006);
 
 SET @2018_bread = (SELECT value
-FROM primary_table AS pt
+FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 WHERE name = 'Chléb konzumní kmínový'
 AND `year` = 2018);
 
 SET @2006_milk = (SELECT value
-FROM primary_table AS pt
+FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 WHERE name = 'Mléko polotučné pasterované'
 AND `year` = 2006);
 
 SET @2018_milk = (SELECT value
-FROM primary_table AS pt
+FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 WHERE name = 'Mléko polotučné pasterované'
 AND `year` = 2018);
 
 -- průměrná mzda v daném odvětví a roce vydělená průměrnými cenami produktů v tomto roce
 WITH payroll_2006 AS (SELECT value, name 
-FROM primary_table AS pt
+FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 WHERE name_type = 'Odvětví'
 AND `year` = 2006),
 	payroll_2018 AS (SELECT value, name 
-FROM primary_table AS pt
+FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 WHERE name_type = 'Odvětví'
 AND `year` = 2018)
 SELECT p06.name,
@@ -101,7 +101,7 @@ SELECT
 	name,
 	`year`,
 	ROUND((value - LAG(value) OVER (PARTITION BY name ORDER BY year)) / ((LAG(value) OVER (PARTITION BY name ORDER BY year)) / 100), 2) AS perc_increase
-FROM primary_table AS pt
+FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 WHERE name_type = 'Produkt')
 -- průměrná hodnota procentuálního narůstu ceny produktů, seřazená od nejmenší k největší
 SELECT 
@@ -119,14 +119,14 @@ WITH
 		name,
 		`year`,
 		ROUND((value - LAG(value) OVER (PARTITION BY name ORDER BY year)) / ((LAG(value) OVER (PARTITION BY name ORDER BY year)) / 100), 2) AS perc_increase_price
-		FROM primary_table AS pt
+		FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 		WHERE name_type = 'Produkt'),
 	increase_wage AS -- procentuální nárůst mzdy v daném odvětví oproti předchozímu roku
 		(SELECT
 		name,
 		`year`,
 		ROUND((value - LAG(value) OVER (PARTITION BY name ORDER BY year)) / ((LAG(value) OVER (PARTITION BY name ORDER BY year)) / 100), 2) AS perc_increase_wage
-		FROM primary_table AS pt
+		FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 		WHERE name_type = 'Odvětví')
 SELECT avg(perc_increase_price) - avg(perc_increase_wage) AS diff_perc_inc, ip.`year` -- rozdíl průměrného narůstu cen produktů v daném roce a průměrného nárůstu mezd všech odvětví
 FROM increase_price AS ip
@@ -150,14 +150,14 @@ WHERE country = 'Czech republic'),
 		name,
 		`year`,
 		ROUND((value - LAG(value) OVER (PARTITION BY name ORDER BY year)) / ((LAG(value) OVER (PARTITION BY name ORDER BY year)) / 100), 2) AS perc_increase_price
-		FROM primary_table AS pt
+		FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 		WHERE name_type = 'Produkt'),
 		wage_inc AS -- procentuální nárůst mzdy v daném odvětví oproti předchozímu roku
 		(SELECT
 		name,
 		`year`,
 		ROUND((value - LAG(value) OVER (PARTITION BY name ORDER BY year)) / ((LAG(value) OVER (PARTITION BY name ORDER BY year)) / 100), 2) AS perc_increase_wage
-		FROM primary_table AS pt
+		FROM t_vojtech_rauer__project_SQL_primary_final AS pt
 		WHERE name_type = 'Odvětví')
 SELECT
 	hi.`year`,
